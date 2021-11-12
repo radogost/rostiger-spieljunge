df6e654132b099b5427ed0f52c5267f8a3773b11
radogost <radogost42@protonmail.com>
2021-11-12 01:50:33 +0100

    Create image for screen

--- gb-native/src/main.rs
+++ gb-native/src/main.rs
@@ -15,8 +15,24 @@ const SCREEN_SIZE: (f32, f32) = (
 
 const DESIRED_FPS: u32 = 60;
 
+const BLACK_SCREEN: [u8; HEIGHT * WIDTH * 4] = [0x00; HEIGHT * WIDTH * 4];
+
 struct GameBoy {
     board: Board,
+    screen: [u8; HEIGHT * WIDTH * 4],
+    param: graphics::DrawParam,
+}
+
+impl GameBoy {
+    fn new(board: Board) -> Self {
+        let scale = graphics::mint::Vector2 { x: 2.0, y: 2.0 };
+        let param = graphics::DrawParam::new().scale(scale);
+        Self {
+            board,
+            screen: BLACK_SCREEN,
+            param,
+        }
+    }
 }
 
 impl ggez::event::EventHandler<ggez::GameError> for GameBoy {
@@ -28,23 +44,20 @@ impl ggez::event::EventHandler<ggez::GameError> for GameBoy {
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
+
         let frame = self.board.frame();
         for (y, row) in frame.into_iter().enumerate() {
             for (x, color) in row.into_iter().enumerate() {
-                let x = x as i32 * PIXEL_SIZE;
-                let y = y as i32 * PIXEL_SIZE;
-                let pixel = graphics::Rect {
-                    x: x as f32,
-                    y: y as f32,
-                    w: PIXEL_SIZE as f32,
-                    h: PIXEL_SIZE as f32,
-                };
-                let color = graphics::Color::from_rgb(color.r, color.g, color.b);
-                let pixel =
-                    graphics::Mesh::new_rectangle(ctx, graphics::DrawMode::fill(), pixel, color)?;
-                graphics::draw(ctx, &pixel, (ggez::mint::Point2 { x: 0.0, y: 0.0 },))?;
+                let idx = (x + y * WIDTH) * 4;
+                self.screen[idx] = color.r;
+                self.screen[idx + 1] = color.g;
+                self.screen[idx + 2] = color.b;
+                self.screen[idx + 3] = 0xFF;
             }
         }
+        let image = graphics::Image::from_rgba8(ctx, WIDTH as u16, HEIGHT as u16, &self.screen)?;
+        graphics::draw(ctx, &image, self.param)?;
 
         graphics::present(ctx)?;
         ggez::timer::yield_now();
@@ -84,9 +97,7 @@ fn main() -> GameResult {
         .window_mode(ggez::conf::WindowMode::default().dimensions(SCREEN_SIZE.0, SCREEN_SIZE.1))
         .build()?;
 
-    let gameboy = GameBoy {
-        board: Board::new(&data),
-    };
+    let gameboy = GameBoy::new(Board::new(&data));
 
     event::run(ctx, event_loop, gameboy);
 }
